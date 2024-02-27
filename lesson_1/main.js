28427f2fbaf9dfdf26172a9105009be26df26f40
CaoKimCam <21520641@gm.uit.edu.vn>
2024-02-27 22:48:26 +0700

    Đối tượng Date
    mozzilla tìm hiểu thêm

---
+++ lesson_1/main.js
@@ -0,0 +1,10 @@
+//Đối tượng Date: tìm hiểu thêm google
+
+var date = new Date();
+
+console.log(date);//trả về string của thời gian hiện tại, như toString
+
+var date = Date(date);//có thể truy xuất getFullYear...,
+//không thể lấy như 1 đối tượng, 
+
+//DATE JAVASCRIPT để tìm hiểu thêm
