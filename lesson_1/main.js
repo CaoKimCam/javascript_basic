6fab227ec6a05387a160c0b0a3d071f6a9e388c7
CaoKimCam <21520641@gm.uit.edu.vn>
2024-02-27 22:53:25 +0700

    Update main.js

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -4,7 +4,8 @@ var date = new Date();
 
 console.log(date);//trả về string của thời gian hiện tại, như toString
 
-var date = Date(date);//có thể truy xuất getFullYear...,
+var date = Date(date);//??syntax
+//có thể truy xuất getFullYear...,
 //không thể lấy như 1 đối tượng, 
 
 //DATE JAVASCRIPT để tìm hiểu thêm
