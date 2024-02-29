ee4925154c5f8a74c9aed0b013917947511846fb
CaoKimCam <21520641@gm.uit.edu.vn>
2024-02-29 23:01:14 +0700

    Làm việc với mảng - P2 (hàm)

---
+++ lesson_1/main.js
@@ -0,0 +1,46 @@
+//làm việc với mảng phần 2
+/**
+ * forEach()
+ * every()
+ * some()
+ * find()
+ * filter()
+ * map()
+ * reduce()
+ */
+var courses=[
+    {
+        id:1,
+        name: 'Javascript',
+        coin: 0
+    },
+    {
+        id:2,
+        name: 'HTML, CSS',
+        coin: 250
+    },
+    {
+        id:1,
+        name: 'NodeJS',
+        coin: 250
+    }
+]
+//forEach(): duyệt qua từng phần tử của mạng
+courses.forEach(function(course, index)//callback
+{
+    console.log(course);
+    // console.log(index, course);//có thể thêm index
+});
+//thay cho dùng vòng lặp
+
+//every:hữu ích kiểm tra các phần tử thoả điều kiện nào đó
+var isFree= courses.every(function(course, index)//callback: gọi ngược lại hòm
+{
+    return course.coin===0
+    //nếu sai dừng luôn, không duyệt nữa
+});
+//cấu trúc các hàm gần tương tự nhau
+
+console.log(isFree);//nếu tất cả khoá học có coin đều =0 trả về true
+
+//some()
