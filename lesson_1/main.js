87b42c9eda5c9f63c21110c5069f52880a8deb9f
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-05 18:24:44 +0700

    del

--- lesson_1/main.js
+++
@@ -1,7 +0,0 @@
-//Document object (đứng đầu mô hình DOM)
-
-//Là đại diện cho cả website
-//Lưu cả vào document, được tích hợp sẵn trong web api của javascript khi chạy trên trình duyệt
-
-document.write('Hello Guys!');//sẽ ghi vào
-
