a9a065513500af69f3681c3a342d236b970b85d4
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-05 18:11:56 +0700

    HTML DOM là gì

---
+++ lesson_1/main.js
@@ -0,0 +1,15 @@
+//HTML DOM là gì
+//Khi 1 web page được tải, trình duyệt tạo 1 Document Object Model
+
+//trình duyệt nhận(chuỗi html) và dịch code ... dịch thành mô hình DOM
+//DOM quy chuẩn làm web cần tuân theo 
+
+//Tạo ra mô hình dạng cây, đgl DOM
+
+/* Có 3 thành phần
+ * 1. Element
+ * 2. Attribute
+ * 3. Text
+ */
+
+// Node (mỗi ô hình vuông)
