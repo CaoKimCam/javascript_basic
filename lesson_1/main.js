4ada85e69b1e39b89e58a5c7688c40931866c567
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-05 18:19:17 +0700

    HTML và DOM API

---
+++ lesson_1/main.js
@@ -0,0 +1,16 @@
+//HTML DOM
+
+// 1. Element
+// 2. Attribute
+// 3. Text
+
+
+// Javascript: cung cấp bộ công cụ truy xuất vào HTML DOM
+//Javascript: Browser | Server (NodeJS)
+// Application programming interface (API)
+//Browser: HTML cần access -> DOM, do đó cần-> DOM API
+
+//DOM API này thuộc WEB API (web api này có nhiều, ví dụ lấy vị trí, nhận thông báo hay không?), chỉ có trên nền tảng hỗ trợ mở trang web
+//Trên server không có, chỉ lưu trữ và xử lí, do đó trên javascript cũng không có, chỉ có browser có hỗ trợ chạy
+
+
