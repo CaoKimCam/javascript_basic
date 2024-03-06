37f832a5344ed4cc5df135ed4997084968d5e7c3
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-06 21:25:45 +0700

    del

--- lesson_1/main.js
+++
@@ -1,4 +0,0 @@
-var a= document.getElementsByTagName//a[0]:để lấy in element
-document.write('hello')//js được thực thi theo vị trí4
-// ngau sau khi document thực thi
-//nếu thực hiện sau khi DOM tải xong, nó ghi đè toàn bộ
