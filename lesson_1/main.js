5740bcd4e0b07c33e55359607d9ca81e5d8bd9da
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-06 20:57:34 +0700

    del

--- lesson_1/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sử dụng các biến đã cho sẵn dưới đây
- */
-
-var productsListElement = document.querySelector('div.products-list');
-var firstProductElement =document.querySelector('div.product:first-child');
-var buttonElements = document.getElementsByTagName('button');
-
-
