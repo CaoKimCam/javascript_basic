35379d2e85cbd32238848a193c2c2ac74e4869f0
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-13 22:08:16 +0700

    Enhanced object literals

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -33,3 +33,42 @@ function logger2(log, isAlert =false){
 // với những giá trị không bắt buộc truyền, ta cần đặt default cho nó
 
 // end: Default parameter values
+
+// Start: Enhanced object literals
+
+// 1. Định nghĩa key: key value cho object
+// 2. Định nghĩa method cho object
+// 3. Định nghĩa key cho object dưới dạng biến
+// cụ thể: cung cấp cách viết ngắn gọn hơn
+
+
+// 1.
+var name='Javascript';
+var price= 1000;
+var course={
+    name,// name: name,
+    price, // price: price
+};
+
+
+// 2.
+var course= {
+    name,
+    price,
+    getName(){//getName: function()
+        return name;
+    }
+}
+
+// 3.
+var fieldName='name';
+var fieldPrice='price';
+
+const course={
+    [fieldName]: name,//name: 'Javascript'
+    [fieldPrice]: price
+}//trường hợp cần tuỳ giá trị biến mà lấy ra value tương ứng thì dùng này
+// còn bình thường vẫn dùng name:...
+
+// End: Enhanced object literals
+
