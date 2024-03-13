9997ed185c92ff5b5ab5995d1f492fde6356ea48
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-13 18:23:47 +0700

    let && const

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -4,3 +4,44 @@
 //Babel
 
 // Let & Const
+// 1. Var/ Let, Const: Scope, hosting
+// 2. Const/ var, let; assignment
+
+// Code block: if else, loop, {},...
+
+if (true){
+    var course ='Javascript basic!';
+}
+
+// Vì là khóa cơ bản, để tránh phức tạp nên chúng ta tạm coi let & const không được hoisted.
+// điểm khác nhau: về phạm vi
+// hosting sẽ được nhấc lên đầu
+
+// Code thuần: Var
+// Babel: Const, Let
+// Khi định nghĩa biến và không gán lại biến đó: const
+// Khi cần gán lại giá trị cho biến: Let
+
+
+/**
+ * var a=1;
+ * 
+ * var a;
+ * a=1;
+ */
+
+const a={
+    name:'Javascript'
+}
+a.name='PHP'
+console.log(a.name);
+
+//có thể thay đổi thuộc tính của hằng, nhưng không thể gán a=
+
+// let isSuccess=false;
+// if(...){
+//     isSuccess=true;
+// }// nếu có gán lại thì dùng let
+// //không có thì dùng const
+
+// End: let & const   
