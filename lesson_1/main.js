6e35fdd17610de5d7d7ceb6e2acc0b6a4aebd576
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-13 22:01:23 +0700

    Default parameter values

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -16,3 +16,20 @@ class Courses{
         this.price = price;
     }
 }
+
+// End: Classes
+
+// Default parameter values: giá trị tham số mặc định
+
+function logger(log='Gia tri mac dinh!'){
+    console.log(log);
+}//để khi không nhập thì lấy giá trị mặc định
+
+function logger2(log, isAlert =false){
+    if(isAlert) return alert(log);
+    console.log(log);
+}
+
+// với những giá trị không bắt buộc truyền, ta cần đặt default cho nó
+
+// end: Default parameter values
