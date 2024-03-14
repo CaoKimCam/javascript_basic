439deb8564698694d0e9fb54d384aca5200b7137
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-15 01:54:10 +0700

    Optional chaining (?.)

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,1 +1,20 @@
 // Optional chaining (?.)
+
+const obj={
+
+    name:'Alice',
+    cat:{
+        name:'Dinah',
+        cat2:{
+            name: 'Dinah2',
+            cat3:{
+                name: 'Dinah3'
+            }
+        }
+    }
+}
+
+console.log(obj?.cat?.cat2?.cat3.name);
+// sử dụng obptional chaining khi không chắc chắc key đó có tồn tại hay khổng?
+// nếu nghi ngờ thêm option chaining vào (?)
+
