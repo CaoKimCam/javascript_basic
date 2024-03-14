5cd82080d186f004fd1f42e937c4e75a2aef3762
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-15 01:43:26 +0700

    del

--- lesson_1/logger.js
+++
@@ -1,6 +0,0 @@
-export const TYPE_LOG='log';
-function logger(log, type='log'){
-    console[type] (log);
-}
-
-export default logger;
--- lesson_1/main.js
+++
@@ -1,6 +0,0 @@
-// Modules: Import / Export
-
-import logger, {TYPE_LOG} from './logger.js';
-
-import * as constants from './constants.js';
-// lấy tất cả 
